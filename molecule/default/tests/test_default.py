6d28441801d7fed72bee5c4ca0fae26f3fcb9a05
Drew Stinnett <drew.stinnett@duke.edu>
2018-10-11 11:49:45 -0400

    Adding in new test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,16 +1,14 @@
-# import os
+import os
 
-# import testinfra.utils.ansible_runner
-# import pytest
+import testinfra.utils.ansible_runner
 
-# testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-# os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# @pytest.mark.parametrize('pkg', [
-# 'docker',
-# ])
-# def test_pkg(host, pkg):
-# package = host.package(pkg)
-#
-# assert package.is_installed
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
